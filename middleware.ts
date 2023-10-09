3c1bd3d39efb2dd85697c0a901042af973f46b43
Jihun (James) Doh <97568255+jihundoh0109@users.noreply.github.com>
2023-10-09 11:30:17 +0000

    clerk webhook

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,23 @@
-import { authMiddleware, redirectToSignIn  } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 export default authMiddleware({
   publicRoutes: ["/"],
+  ignoredRoutes: ["/api/webhook"],
   afterAuth(auth, req, _evt) {
-    // handle users who aren't authenticated 
+    // handle users who aren't authenticated
     if (!auth.userId && !auth.isPublicRoute) {
-      return NextResponse.redirect("https://friendly-eureka-x6jxvj44qgcvwwx-3000.app.github.dev/signin");
+      return NextResponse.redirect(
+        "https://friendly-eureka-x6jxvj44qgcvwwx-3000.app.github.dev/signin"
+      );
     }
     // handle users who are authenticated but shouldn't be on a public route ("/" is fine)
     if (auth.userId && auth.isPublicRoute && req.nextUrl.pathname !== "/") {
-      return NextResponse.redirect("https://friendly-eureka-x6jxvj44qgcvwwx-3000.app.github.dev/");
+      return NextResponse.redirect(
+        "https://friendly-eureka-x6jxvj44qgcvwwx-3000.app.github.dev/"
+      );
     }
-  }
+  },
 });
 
 export const config = {
