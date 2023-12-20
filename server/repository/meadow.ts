230a13c23fe985e811f0d23356eccc969a8b710e
Jihun (James) Doh <97568255+jihundoh0109@users.noreply.github.com>
2023-12-20 10:56:57 +0000

    log

--- server/repository/meadow.ts
+++ server/repository/meadow.ts
@@ -14,6 +14,7 @@ export async function getMeadows() {
 }
 
 export async function getMeadowById(meadowId: string) {
+  console.log("getMeadowById")
   const meadow = await prisma.meadow.findUnique({
     where: {
       id: meadowId,
@@ -23,5 +24,6 @@ export async function getMeadowById(meadowId: string) {
       users: true,
     },
   });
+  console.log(meadow);
   return meadow;
 }
