a415d5fa5fe109b377a53d2a9ddffa7738e88e39
Jihun (James) Doh <97568255+jihundoh0109@users.noreply.github.com>
2023-12-20 10:46:44 +0000

    prisma

--- lib/db.ts
+++ lib/db.ts
@@ -4,7 +4,7 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma =
+const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     log:
@@ -14,3 +14,5 @@ export const prisma =
   });
 
 if (process.env.NODE_ENV === "production") globalForPrisma.prisma = prisma;
+
+export { prisma }
