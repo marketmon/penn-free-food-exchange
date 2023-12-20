401578f47bf5d435da506151870618bf97f55b56
Jihun (James) Doh <97568255+jihundoh0109@users.noreply.github.com>
2023-12-20 10:38:19 +0000

    prisma fix

--- lib/db.ts
+++ lib/db.ts
@@ -1,16 +1,14 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+let prisma: PrismaClient;
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log:
-      process.env.NODE_ENV === "development"
-        ? ["query", "info", "warn"]
-        : ["warn"],
-  });
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 
-if (process.env.NODE_ENV === "production") globalForPrisma.prisma = prisma;
+export { prisma };
